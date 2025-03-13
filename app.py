2a0fc35f0b3228921d4c0a8c7cae3325ebf99bb1
amirdavari <93599040+AmirmahdiDavari@users.noreply.github.com>
2025-03-14 01:21:35 +0330

    a

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+# from pyngrok import ngrok
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return render_template("test.html")
+
+
+if __name__ == "__main__":
+    # # ngrok.set_auth_token("2uHI6Rn7WxHxXq7Sy25DaNp1KzQ_22S8KCsMwQNfnQJuAHJ7h")
+    # ngrok_tunnel = ngrok.connect(5000)
+    # print(f"Public URL: {ngrok_tunnel.public_url}")
+
+    app.run(port=5000)
+    # app.run(debug=True)
